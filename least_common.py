c5d32d847e253bff946125b71d13b15b99ae2734
jsimonton020 <jsimonton020@gmail.com>
2016-09-01 16:06:47 -0700

    python exercises

--- least_common.py
+++ least_common.py
@@ -2,7 +2,17 @@
 
 def get_numbers():
     with open("numbers.txt") as n:
-      numbers = n.readline()
-    return(numbers)
+        numbers = n.read().splitlines()
+        lcd = []
+        for i in numbers:
+            x, y = i.split()
+            x = int(x)
+            y = int(y)
+            small = min(x, y)
+            for j in range(small, 1, -1):
+                if x % j == 0 and y % j == 0:
+                    lcd.append((x // j, y // j))
+                    break
+    return(lcd)
 
-print(numbers)
+print (get_numbers())
