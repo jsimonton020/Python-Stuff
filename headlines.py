f4557536c90e7a0850d127769e14b27d379c6442
jsimonton020 <jsimonton020@gmail.com>
2016-09-07 14:01:25 -0700

    this is a comment

--- headlines.py
+++ headlines.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 import requests
+import textwrap
 from bs4 import BeautifulSoup
 
 url = 'http://www.nytimes.com'
 r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+title = soup.find_all(class_="story-heading")
+
+for i in title:
+    headlines = i.get_text()
+    headlines = textwrap.fill(headlines, width=70).strip()
+    print(headlines)
