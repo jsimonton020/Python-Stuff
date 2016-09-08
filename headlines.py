74caf2bf4eace3cb18e7a90dde5c7ece88e6b5bf
jsimonton020 <jsimonton020@gmail.com>
2016-09-08 15:26:24 -0700

    presidents challenge

--- headlines.py
+++ headlines.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 import requests
 import textwrap
 from bs4 import BeautifulSoup
@@ -8,7 +9,12 @@ r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 title = soup.find_all(class_="story-heading")
 
+headlines_list = []
+
 for i in title:
-    headlines = i.get_text()
-    headlines = textwrap.fill(headlines, width=70).strip()
+    headlines = i.get_text().strip()
+    headlines_list.append(headlines)
     print(headlines)
+
+with open('headlines.json', 'w') as w:
+    json.dump(headlines_list, w, indent=4)
